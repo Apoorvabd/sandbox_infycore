fbd338bf87b3c148652590c8ec2d9eb70e94a752
apoorv <mrapoorvchaturvedi7@gmail.com>
2026-06-24 16:18:28 +0530

    chore: add startup log for mock bank server

--- mock_server/src/server.js
+++ mock_server/src/server.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 4000;
-
+console.log(
+    `Mock Bank server is on now`
+);
 app.listen(PORT, () => {
     console.log(
         `Mock Bank Server Running On Port ${PORT}`
