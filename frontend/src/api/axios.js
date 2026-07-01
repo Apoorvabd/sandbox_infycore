2b43982295893b14861dd8fce0d9e84b148a201e
apoorv <mrapoorvchaturvedi7@gmail.com>
2026-07-01 17:15:05 +0530

    frontend checked

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Read API URL from Vite environment
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 console.log("API Base URL:", BASE_URL);
 const api = axios.create({
