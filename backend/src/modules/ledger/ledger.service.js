360bcede4ad85fb25fdb8f050e03501ebe0132e2
apoorv <mrapoorvchaturvedi7@gmail.com>
2026-07-01 15:06:58 +0530

    ledger modified

--- backend/src/modules/ledger/ledger.service.js
+++ backend/src/modules/ledger/ledger.service.js
@@ -8,50 +8,27 @@ import {
     getLedgerSummary
 }
 from "./ledger.repository.js";
-export const getTransaction =
-async (id) => {
+export const getTransaction = async (id) => {
 
     return await getTransactionById(id);
 };
-export const ledgerSummary =
-async () => {
+
+export const ledgerSummary = async () => {
 
     return await getLedgerSummary();
 };
-const getLedger =
-async (query) => {
-
+const getLedger = async (query) => {
     return await getLedgerTransactions({
-
-        page:
-            Number(query.page) || 1,
-
-        limit:
-            Number(query.limit) || 10,
-
-        category:
-            query.category,
-
-        merchant:
-            query.merchant,
-
-        direction:
-            query.direction,
-
-        search:
-            query.search,
-
-        from:
-            query.from,
-
-        to:
-            query.to,
-
-        sort:
-            query.sort,
-
-        order:
-            query.order
+        page: Number(query.page) || 1,
+        limit: Number(query.limit) || 10,
+        category: query.category,
+        merchant: query.merchant,
+        direction: query.direction,
+        search: query.search,
+        from: query.from,
+        to: query.to,
+        sort: query.sort,
+        order: query.order
     });
 };
 export default  getLedger ;
